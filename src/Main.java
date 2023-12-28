6e3edaf1046cc8be1dcd40a50e21650c8bd428f5
yalexaner <mail@yalexaner.ru>
2023-12-28 20:49:47 +0500

    generate collections once

--- src/Main.java
+++ src/Main.java
@@ -6,10 +6,7 @@ import structure.Stack;
 
 import java.time.Duration;
 import java.time.Instant;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
@@ -48,30 +45,38 @@ public class Main {
 
             System.out.println();
 
-            for (BaseSort algorithm : sortingAlgorithms) {
-                if (input == 1) {
-                    ArrayList<Integer> array = ArrayGenerator.generate(elementsAmount);
-                    sortCollection(array, algorithm);
-                } else if (input == 2) {
-                    List<Integer> list = ListGenerator.generate(elementsAmount);
-                    sortCollection(list, algorithm);
-                } else if (input == 3) {
-                    HashMap<Integer, Integer> map = MapGenerator.generate(elementsAmount);
-                    sortCollection(map, algorithm);
-                } else if (input == 4) {
-                    Queue queue = QueueGenerator.generate(elementsAmount);
-                    sortCollection(queue, algorithm);
-                } else if (input == 5) {
-                    Stack stack = StackGenerator.generate(elementsAmount);
-                    sortCollection(stack, algorithm);
-                } else {
-                    break;
-                }
+            if (input == 1) {
+                ArrayList<Integer> array = ArrayGenerator.generate(elementsAmount);
+                sort(array);
+            } else if (input == 2) {
+                List<Integer> list = ListGenerator.generate(elementsAmount);
+                sort(list);
+            } else if (input == 3) {
+                HashMap<Integer, Integer> map = MapGenerator.generate(elementsAmount);
+                sort(map);
+            } else if (input == 4) {
+                Queue queue = QueueGenerator.generate(elementsAmount);
+                sort(queue);
+            } else if (input == 5) {
+                Stack stack = StackGenerator.generate(elementsAmount);
+                sort(stack);
+            } else {
+                break;
             }
 
             System.out.println("\nНажмите ENTER для продолжения...");
             scanner.nextLine();
             System.out.println();
+        }
+    }
+
+    private static void sort(List<Integer> collection) {
+        for (BaseSort algorithm : sortingAlgorithms) {
+            if (collection instanceof ArrayList<Integer>) {
+                sortCollection(new ArrayList<>(collection), algorithm);
+            } else {
+                sortCollection(new LinkedList<>(collection), algorithm);
+            }
         }
     }
 
@@ -85,6 +90,12 @@ public class Main {
         System.out.println("Сортировка заняла " + Duration.between(start, end).toMillis() / 1000f + " секунд");
     }
 
+    private static void sort(HashMap<Integer, Integer> collection) {
+        for (BaseSort algorithm : sortingAlgorithms) {
+            sortCollection(new HashMap<>(collection), algorithm);
+        }
+    }
+
     private static void sortCollection(HashMap<Integer, Integer> map, BaseSort algorithm) {
         System.out.println("Сортировка с помощью алгоритма " + algorithm.getClass().getSimpleName());
 
@@ -93,6 +104,12 @@ public class Main {
         Instant end = Instant.now();
 
         System.out.println("Сортировка заняла " + Duration.between(start, end).toMillis() / 1000f + " секунд");
+    }
+
+    private static void sort(BaseStructure collection) {
+        for (BaseSort algorithm : sortingAlgorithms) {
+            sortCollection(collection.copy(), algorithm);
+        }
     }
 
     private static void sortCollection(BaseStructure collection, BaseSort algorithm) {
